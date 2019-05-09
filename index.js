d0b559465941ec101ff67e26e4dae32b3d12f696
Cobrettie <cobrettiegarner3@gmail.com>
2019-05-08 20:57:07 -0400

    Add code example for index.js

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
 // STEP 3: Create the new Class
 class Link {
     constructor(link) {
-        console.log(link);
+        // console.log(link);
+        // console.log(this.link.dataset.tab);
         this.link = link;
+        this.content = document.querySelector(`.content[data-tab='${this.link.dataset.tab}']`);
+        // console.log(this.content);
+        this.content = new Content(this.content);
+        this.link.addEventListener('click', () => this.linkClick());
     }
     // methods
-
+    linkClick() {
+        this.content.toggleContent();
+    }
 }
 
+class Content {
+    constructor(content) {
+        this.contentTab = content;
+    }
+    // methods
+    toggleContent() {
+        this.contentTab.classList.toggle('change');
+    }
+}
 
 // STEP 1: Select the DOM elements
-const links = document.querySelectorAll('.links');
-console.log(links);
-
+const links = document.querySelectorAll('.link');
+// console.log(links);
 
 // STEP 2: Use a loop to return the new Class
 links.forEach(link => {
     // console.log(link);
     return new Link(link);
-})
+});
